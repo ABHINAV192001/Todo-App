658579a5efc41b3b3237a604c15fe081c1822c14
Abhinav Sahu <abhinavsahu120@gmail.com>
2025-04-02 02:31:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import Header from "./Parts/Header";
+import { Todos } from "./Parts/Todos";
+
+import { AddTodo } from "./Parts/AddTodo";
+
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e) => e !== todo));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const addTodo = (title, desc) => {
+    let sno = todos.length === 0 ? 0 : todos[todos.length - 1].sno + 1;
+    const myTodo = { sno, title, desc };
+    setTodos([...todos, myTodo]);
+  };
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <Router>
+      <Header title="My Todos List"  />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddTodo addTodo={addTodo} />
+              <Todos todos={todos} onDelete={onDelete} />
+            </>
+          }
+          />
+          </Routes>
+
+
+    </Router>
+  );
+}
+
+export default App;
